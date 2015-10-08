3df5248495027348e8a07ff7c2845bdf9b0be88e
Pavankumar Arepu <iosdeveloper.ipa@gmail.com>
2015-10-08 22:51:35 +0530

    Initial Commit

---
+++ PlistExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlistExample
+//
+//  Created by Pavankumar Arepu on 10/8/15.
+//  Copyright © 2015 Pavankumar Arepu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlistExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlistExample
+//
+//  Created by Pavankumar Arepu on 10/8/15.
+//  Copyright © 2015 Pavankumar Arepu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
